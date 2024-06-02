1f675aaddc95ab532611792216c12fbe60e5e160
alpha-abhii <alpha20abhi@gmail.com>
2024-06-02 12:51:39 +0530

    Project setup

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	app := fiber.New()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
+	log.Fatal(app.Listen(":"+PORT))
+}
